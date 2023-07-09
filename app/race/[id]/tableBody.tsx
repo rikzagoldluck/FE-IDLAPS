f1ab565c022b531c1e09474c3a62039733ebb47e
rikzagoldluck <rikzasimdigei@gmail.com>
2023-07-09 10:44:27 +0700

    FIXING

--- app/race/[id]/tableBody.tsx
+++ app/race/[id]/tableBody.tsx
@@ -1,15 +1,18 @@
 "use client";
 
 import { SelectBoxContext } from "@/app/provider/SelectBox";
+import Navbar from "@/components/Navbar";
 import SelectRiderNote from "@/components/SelectRiderNote";
 import { convertDateTimeMillis, timeDifference } from "@/services/converter";
 import { getRidersRunInCategory } from "@/services/riders";
 import { Rider } from "@/services/riders/data-type";
 import { useContext } from "react";
+import { Toaster } from "react-hot-toast";
 import useSWR from "swr";
 
-export default function TableBody({ id }: { id: number }) {
+export default function Page({ id }: { id: number }) {
   const selectBox = useContext(SelectBoxContext);
+
   //   const router = useRouter();
   const { data, error, isLoading } = useSWR("getRidersRunInCategory", () =>
     getRidersRunInCategory(id)
@@ -22,26 +25,34 @@ export default function TableBody({ id }: { id: number }) {
     return <></>;
   }
   if (!data) return <></>;
+  console.log();
 
   if (data.length === 0) {
     return (
-      <table className="table table-zebra w-full font-bold text-center">
-        <thead>
-          <tr>
-            <th>POS</th>
-            <th>Rider</th>
-            <th>Team</th>
-            <th>BIB</th>
-            <th>TIME</th>
-            <th>GAP</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td colSpan={6}>No Data</td>
-          </tr>
-        </tbody>
-      </table>
+      <>
+        <Navbar title={"Category Championship"} />
+        <div className="py-10 px-10">
+          <div className="overflow-x-auto">
+            <table className="table table-zebra w-full font-bold text-center">
+              <thead>
+                <tr>
+                  <th>POS</th>
+                  <th>Rider</th>
+                  <th>Team</th>
+                  <th>BIB</th>
+                  <th>TIME</th>
+                  <th>GAP</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td colSpan={6}>No Data</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </>
     );
   }
   const riders: Rider[] = data;
@@ -49,7 +60,7 @@ export default function TableBody({ id }: { id: number }) {
   const start_time = riders[0].categories.start_time;
   const categoryName = riders[0].categories.name;
 
-  if (riders[0].race_results.length > 1) {
+  if (riders[0].race_results.length != 0) {
     riders.sort((a, b) => {
       // Urutan berdasarkan jumlah lap terbanyak (descending)
       if (a.race_results.length !== b.race_results.length) {
@@ -57,16 +68,14 @@ export default function TableBody({ id }: { id: number }) {
       }
 
       // Jika jumlah lap sama, urutan berdasarkan waktu tercepat (ascending)
-      if (a.race_results[a.race_results.length - 1].finish_time) {
-        if (
-          a.race_results[a.race_results.length - 1].finish_time !==
-          b.race_results[b.race_results.length - 1].finish_time
-        ) {
-          return (
-            Number(a.race_results[a.race_results.length - 1].finish_time) -
-            Number(b.race_results[b.race_results.length - 1].finish_time)
-          );
-        }
+      if (
+        a.race_results[a.race_results.length - 1].finish_time !==
+        b.race_results[b.race_results.length - 1].finish_time
+      ) {
+        return (
+          Number(a.race_results[a.race_results.length - 1].finish_time) -
+          Number(b.race_results[b.race_results.length - 1].finish_time)
+        );
       }
     });
 
@@ -104,7 +113,7 @@ export default function TableBody({ id }: { id: number }) {
           <tr key={index}>
             <td>{index + 1}</td>
             <td>{pembalap.name}</td>
-            <td>{pembalap.team_name}</td>
+            <td>{pembalap.teams.name}</td>
             <td>{pembalap.bib}</td>
             <td>{convertDateTimeMillis(start_time)}</td>
             {pembalap.total_waktu === "0" && <td>00:00:00.000</td>}
